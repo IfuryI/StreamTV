72dee29532a957cbff49bf394a724346d497e1a1
let-robots-reign <zotov.alexei.1@yandex.ru>
2021-10-29 12:09:31 +0300

    logout button

--- app/src/main/java/com/example/streamtv/Profile.kt
+++ app/src/main/java/com/example/streamtv/Profile.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 
 class Profile : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,10 +20,19 @@ class Profile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.profile, container, false)
+
         val editButton = rootView.findViewById<ImageView>(R.id.editButton)
+        val logoutButton = rootView.findViewById<ImageView>(R.id.logoutButton)
+
         editButton.setOnClickListener {
             Toast.makeText(activity, "Editing profile in progress...", Toast.LENGTH_SHORT).show()
         }
+
+        logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            rootView.findNavController().navigate(R.id.action_profileFragment_to_login)
+        }
+
         return rootView
     }
 }
